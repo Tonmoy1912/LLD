fc2e2895bb30fc71ec08258e10450aef1993a817
Tonmoy Biswas <tonmoybiswas19122002@gmail.com>
2026-07-27 14:38:12 +0530

    Rules for TicTacToeBoard moved into itself

--- Java/turned-based-game-ai-engine/src/main/java/com/tonmoy1912/api/RuleEngine.java
+++ Java/turned-based-game-ai-engine/src/main/java/com/tonmoy1912/api/RuleEngine.java
@@ -2,8 +2,6 @@ package com.tonmoy1912.api;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import com.tonmoy1912.boards.TicTacToeBoard;
 import com.tonmoy1912.game.Board;
@@ -22,27 +20,7 @@ public class RuleEngine {
 
     public RuleEngine() {
         String key = TicTacToeBoard.class.getName();
-        map.put(key, new RuleSet<>());
-        map.get(key).add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(i, j))));
-        map.get(key).add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(j, i))));
-        map.get(key).add(new Rule<TicTacToeBoard>(board -> traverse(i -> board.getSymbol(i, i))));
-        map.get(key).add(new Rule<TicTacToeBoard>(board -> traverse(i -> board.getSymbol(i, 2 - i))));
-        map.get(key).add(new Rule<TicTacToeBoard>(board -> {
-            int countOfFilledCells = 0;
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    if (board.getSymbol(i, j) != null) {
-                        countOfFilledCells++;
-                    }
-                }
-            }
-
-            if (countOfFilledCells == 9) {
-                return new GameState(true, "-");
-            } else {
-                return new GameState(false, "-");
-            }
-        }));
+        map.put(key, TicTacToeBoard.getRules() );
     }
 
     public GameState getState(Board board) {
@@ -115,34 +93,4 @@ public class RuleEngine {
             throw new IllegalArgumentException();
         }
     }
-
-    private GameState traverse(Function<Integer, String> next) {
-        GameState result = new GameState(false, "-");
-        boolean possibleStreak = true;
-        for (int i = 0; i < 3; i++) {
-            if (next.apply(i) == null || !next.apply(i).equals(next.apply(0))) {
-                possibleStreak = false;
-                break;
-            }
-        }
-        if (possibleStreak) {
-            result = new GameState(true, next.apply(0));
-        }
-
-        return result;
-    }
-
-    private GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
-        GameState result = new GameState(false, "-");
-        for (int i = 0; i < 3; i++) {
-            int ii = i;
-            GameState traversal = traverse((j) -> next.apply(ii, j));
-            if (traversal.isOver()) {
-                result = traversal;
-                break;
-            }
-        }
-        return result;
-    }
-
 }
--- Java/turned-based-game-ai-engine/src/main/java/com/tonmoy1912/boards/TicTacToeBoard.java
+++ Java/turned-based-game-ai-engine/src/main/java/com/tonmoy1912/boards/TicTacToeBoard.java
@@ -1,11 +1,42 @@
 package com.tonmoy1912.boards;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 import com.tonmoy1912.game.Board;
 import com.tonmoy1912.game.Cell;
+import com.tonmoy1912.game.GameState;
 import com.tonmoy1912.game.Move;
+import com.tonmoy1912.game.Rule;
+import com.tonmoy1912.game.RuleSet;
 
 public class TicTacToeBoard implements Board {
     private String cells[][] = new String[3][3];
+
+    public static RuleSet<TicTacToeBoard> getRules() {
+        RuleSet<TicTacToeBoard> ruleSet = new RuleSet<>();
+        ruleSet.add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(i, j))));
+        ruleSet.add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(j, i))));
+        ruleSet.add(new Rule<TicTacToeBoard>(board -> traverse(i -> board.getSymbol(i, i))));
+        ruleSet.add(new Rule<TicTacToeBoard>(board -> traverse(i -> board.getSymbol(i, 2 - i))));
+        ruleSet.add(new Rule<TicTacToeBoard>(board -> {
+            int countOfFilledCells = 0;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board.getSymbol(i, j) != null) {
+                        countOfFilledCells++;
+                    }
+                }
+            }
+
+            if (countOfFilledCells == 9) {
+                return new GameState(true, "-");
+            } else {
+                return new GameState(false, "-");
+            }
+        }));
+        return ruleSet;
+    }
 
     public String getSymbol(int row, int col) {
         return cells[row][col];
@@ -38,12 +69,42 @@ public class TicTacToeBoard implements Board {
     // Prototype Design Pattern
     @Override
     public TicTacToeBoard copy() {
-        TicTacToeBoard boardCopy= new TicTacToeBoard();
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                boardCopy.cells[i][j]=cells[i][j];
+        TicTacToeBoard boardCopy = new TicTacToeBoard();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardCopy.cells[i][j] = cells[i][j];
             }
         }
         return boardCopy;
     }
+
+    private static GameState traverse(Function<Integer, String> next) {
+        GameState result = new GameState(false, "-");
+        boolean possibleStreak = true;
+        for (int i = 0; i < 3; i++) {
+            if (next.apply(i) == null || !next.apply(i).equals(next.apply(0))) {
+                possibleStreak = false;
+                break;
+            }
+        }
+        if (possibleStreak) {
+            result = new GameState(true, next.apply(0));
+        }
+
+        return result;
+    }
+
+    private static GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
+        GameState result = new GameState(false, "-");
+        for (int i = 0; i < 3; i++) {
+            int ii = i;
+            GameState traversal = traverse((j) -> next.apply(ii, j));
+            if (traversal.isOver()) {
+                result = traversal;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
